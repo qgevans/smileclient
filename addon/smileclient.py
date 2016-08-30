1c6f42eba9eed23be7706d459722a9128707a16b
Quinn Evans <qgevans@gmail.com>
2016-08-30 16:58:48 -0600

    Initial commit.

---
+++ addon/smileclient.py
@@ -0,0 +1,12 @@
+__module_name__ = "smileclient"
+__module_version__ = "0.1"
+__module_description__ = "Loader for smileclient"
+
+import hexchat
+smileclient_path = hexchat.get_info("configdir") + "/addons/smileclient/"
+
+def load_module(module):
+    hexchat.command("py load " + smileclient_path + module + ".py")
+    hexchat.hook_unload(lambda current_module: hexchat.command("py unload smileclient-" + module))
+
+load_module("hello")
---
+++ addon/smileclient/hello.py
@@ -0,0 +1,5 @@
+__module_name__ = "smileclient-hello"
+__module_version__ = "0.1"
+__module_description__ = "Says \"Hello, world!\" when smileclient loads"
+
+print("Hello, world!")
